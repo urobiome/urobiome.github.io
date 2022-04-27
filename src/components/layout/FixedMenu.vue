311c571f868b55ab4b9e8a49c87c1d884d1b9559
urobiome <104316976+urobiome@users.noreply.github.com>
2022-04-28 08:11:29 +0900

    Add files via upload

---
+++ src/components/layout/FixedMenu.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <b-nav tabs justified class="fixedMenu">
+            <b-nav-item active><router-link to='/main'><i class="fi fi-sr-home"></i><i class="fi fi-rr-home"></i></router-link></b-nav-item>
+            <b-nav-item><router-link to='/urobiome'><i class="fi fi-sr-fill"></i><i class="fi fi-rr-fill"></i></router-link></b-nav-item>
+            <b-nav-item><router-link to='/mypage'><i class="fi fi-sr-user"></i><i class="fi fi-rr-user"></i></router-link></b-nav-item>
+        </b-nav>
+    </div>
+</template>
+<script>
+export default {
+    name: 'FixedMenu'
+}
+
+</script>
